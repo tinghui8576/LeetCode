d35da51e7cbaeff79e313466c21ee213843090d0
Ting Hui Cheng <tinghui1738cheng@gmail.com>
2023-06-12 12:55:31 +0800

    Time: 128 ms (42.66%), Space: 50.7 MB (41.93%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] sort(int[] arr){
+        Arrays.sort(arr);
+        return arr;
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        nums = sort(nums);
+        // for (int i = 0 ; i < nums.length; i++){
+        //     System.out.println(nums[i]);
+        // }
+        int a = Integer.MAX_VALUE;
+        
+        int i = 0;
+        while ( i < nums.length){
+            
+            if (nums[i] != a){
+                
+                int j = i +1;
+                int k = nums.length -1;
+                int b = Integer.MAX_VALUE;
+                while ( j < k){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    // System.out.print(nums[i]);
+                    // System.out.print(nums[j]);
+                    
+                    // System.out.print(nums[k]);
+                    if (nums[j] != b){
+                        if(sum == 0){
+                            a = nums[i];
+                            b = nums[j];
+                            ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                            j++;
+                            k = nums.length-1;
+                        }
+                        else if(sum > 0){
+                            k --;
+                        }
+                        else if (sum < 0){
+                            j ++;
+                        }
+                    }
+                    else{
+                        j++;
+                        k = nums.length-1;
+                    }
+                    
+                   
+                }
+            }
+            i++;
+    
+        }
+        return ans;
+    }
+}
