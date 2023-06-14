1f279aa31ddcd7c5a064dc6f14f1c8bba18bc0dc
Ting Hui Cheng <tinghui1738cheng@gmail.com>
2023-06-15 00:06:25 +0800

    Time: 4 ms (82.47%), Space: 43.7 MB (23.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        return String.valueOf(ss).equals(String.valueOf(tt));
+    }
+}
